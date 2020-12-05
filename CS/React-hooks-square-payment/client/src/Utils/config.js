2c92f319fe6e86d0c58d619e109524c0b53bfa0d
Yamogocha <yamogocha@yahoo.com>
2020-12-05 00:26:38 -0800

    clean up utils comments

--- CS/React-hooks-square-payment/client/src/Utils/config.js
+++ CS/React-hooks-square-payment/client/src/Utils/config.js
@@ -2,9 +2,6 @@
 import { methodsSupported, cardNonceResponseReceived, inputEventReceived } from './helper'
 
 const config = {
-    // Initialize the paymentForm elements
-
-    // applicationId: process.env.REACT_APP_APLLICATION_ID,
     applicationId: 'sandbox-sq0idb-JDxB0RhjQ0X2SRyJN9VZsw',
     locationId: "LBMEPJSGHG1EN",
     inputClass: 'sq-input',
@@ -18,7 +15,6 @@ const config = {
     googlePay: {
       elementId: 'sq-google-pay'
     },
-    // Customize the CSS for SqPaymentForm iframe elements
     inputStyles: [{
         fontSize: '16px',
         lineHeight: '24px',
@@ -27,7 +23,6 @@ const config = {
         placeholderColor: 'white',
         backgroundColor: 'transparent'
     }],
-    // Initialize the credit card placeholders
     cardNumber: {
         elementId: 'sq-card-number',
         placeholder: 'Card Number'
@@ -44,14 +39,9 @@ const config = {
         elementId: 'sq-postal-code',
         placeholder: 'Postal'
     },
-    // SqPaymentForm callback functions
     callbacks: {
       methodsSupported: (methods) => methodsSupported(methods),
       createPaymentRequest: () => {},
-        /*
-        * callback function: cardNonceResponseReceived
-        * Triggered when: SqPaymentForm completes a card nonce request
-        */
         cardNonceResponseReceived: (errors, nonce) => cardNonceResponseReceived(errors, nonce),
         inputEventReceived: (inputEvent) => inputEventReceived(inputEvent)
       }
